237da919d835fee68a1b0d5cc9fb38d32db80faf
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-10-24 21:26:06 +0530

    learn about the <Image> component

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
-import { View, Text } from 'react-native'
-
+import { View, Text, Image, ImageBackground } from 'react-native'
+import image from './assets/adaptive-icon.png'
 export default function App() {
-  return <View style={{ flex: 1, backgroundColor: "plum", padding:100 }}>
-    <Text><Text style={{color:"white"}}>Hello</Text> World</Text>
+  return (
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 50 }}>
+      {/* <Text><Text style={{ color: "white" }}>Hello</Text> World</Text> */}
+      {/* <Image source={image} style={{width: 250, height: 300}} /> */}
+      <Image source={{uri:'https://images.pexels.com/photos/16549340/pexels-photo-16549340/free-photo-of-distant-view-on-templo-de-san-antonio-aguascalientes-mexico.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'}} style={{width: 300, height: 300}} />
+  <ImageBackground source={{uri:'https://images.pexels.com/photos/16549340/pexels-photo-16549340/free-photo-of-distant-view-on-templo-de-san-antonio-aguascalientes-mexico.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'}} style={{flex:1, display:'flex', justifyContent:'center', alignItems:'center'}}>
 
-  </View>
+        <Text style={{color: "white"}}>This is foreign out of you budget</Text>
+
+  </ImageBackground>
+    </View>)
 }
