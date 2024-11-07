f4f3952acafc40b7061d220281fc5c6d0492bcdf
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-07 14:33:02 +0530

    onLongPress and PressIn and PressOut

--- Pressables.js
+++ Pressables.js
@@ -1,12 +1,11 @@
 import { View, Pressable, Text, Image } from 'react-native';
-import img from './assets/splash.png'
+import img from './assets/adaptive-icon.png'
 
 const Pressables = () => {
     return (
-        <View>
-
-            <Pressable onPress={()=>console.log("image has pressed")}>
-                <Image source={img} style={{width:300, height: 400}} />
+        <View style={{padding:0}}>
+            <Pressable onPressIn={()=>console.log("image has pressed")}>
+                <Image source={img} style={{width:300, height:300}} />
             </Pressable>
             <Pressable onPress={()=>console.log("text has pressed")}>
                 <Text>
