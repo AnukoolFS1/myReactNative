c3a84feaa4801b7266b85935ab638e23993b5b09
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-07 13:59:24 +0530

    Learn a button component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, ImageBackground } from 'react-native'
+import { View, Text, Image, ImageBackground, Button } from 'react-native'
 import ScrollViewC from './ScrollVeiw'
 export default function App() {
   return (
@@ -9,7 +9,8 @@ export default function App() {
       {/* <ImageBackground source={{ uri: 'https://images.pexels.com/photos/16549340/pexels-photo-16549340/free-photo-of-distant-view-on-templo-de-san-antonio-aguascalientes-mexico.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' }} style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ color: "white" }}>This is foreign out of you budget</Text>
       </ImageBackground> */}
-      <ScrollViewC />
+      {/* <ScrollViewC /> */}
+        <Button title='Button' />
     </View>
   )
 }
