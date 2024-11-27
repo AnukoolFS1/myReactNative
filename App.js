bfe0d3da2111e407957ee4ff4e0c9e66ea8baa0f
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-27 13:01:22 +0530

    recalled the Text Component

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { View } from 'react-native';
+import Writing from './Writing';
 
 export default function App() {
   return (
-    <View>
-
+    <View style={{flex:1,justifyContent:"center", alignItems:"center"}}>
+      <Writing />
     </View>
   )
 }
---
+++ Writing.js
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+const Writing = () => {
+
+    return (
+        <Text>
+            This is a text component
+        </Text>
+    )
+}
+
+
+export default Writing
