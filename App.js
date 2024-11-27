1c430a840a4bf0f93b29c1f5e1803f08ad351309
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-27 17:53:42 +0530

    Button Component

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { View } from 'react-native';
 import Writing from './Writing';
 import Picture from './Picture';
+import Pressive from './Pressive';
 
 export default function App() {
   return (
     <View style={{flex:1,display:"flex",justifyContent:"center", alignItems:"center", backgroundColor:"cornflowerblue"}}>
       <Writing />
-      <Picture />
+      <Pressive />
+      {/* <Picture /> */}
     </View>
   )
 }
---
+++ Pressive.js
@@ -0,0 +1,12 @@
+import { Button } from "react-native";
+
+const Pressive = () => {
+
+    return (
+        <>
+        <Button title="button" onPress={()=>console.log('hello user')} />
+        </>
+    )
+}
+
+export default Pressive
