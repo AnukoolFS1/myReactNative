dfa8af7f07f7937e542cbc193422debc64508754
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-27 12:56:21 +0530

    restarting the react native to recall the learnings

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-import { View, Text, Image, ImageBackground, Button } from 'react-native'
-import ScrollViewC from './ScrollVeiw'
-import Pressables from './Pressables'
-import CButton from './Button'
-import Modals from './Modal'
+import { View } from 'react-native';
+
 export default function App() {
   return (
+    <View>
 
-
-    <View style={{ flex: 1, backgroundColor: "limegreen", padding: 40 }}>
-      {/* <Text><Text style={{ color: "white" }}>Hello</Text> World</Text> */}
-      {/* <ImageBackground source={{ uri: 'https://images.pexels.com/photos/16549340/pexels-photo-16549340/free-photo-of-distant-view-on-templo-de-san-antonio-aguascalientes-mexico.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' }} style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ color: "white" }}>This is foreign out of you budget</Text>
-      </ImageBackground> */}
-      {/* <ScrollViewC /> */}
-      <CButton />
-      <Pressables />
-      <Modals />
     </View>
   )
 }
