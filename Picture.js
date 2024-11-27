6375c117d7782ba76c2828e1c0099c8d07b78e49
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-27 13:21:25 +0530

    Image with uri

--- Picture.js
+++ Picture.js
@@ -4,7 +4,10 @@ import img from './assets/adaptive-icon.png'
 const Picture = () => {
 
     return (
-        <Image source={img} style={{width:300, height:300}}/>
+        <>
+            <Image source={img} style={{ width: 300, height: 300 }} />
+            <Image source={{uri:"https://images.pexels.com/photos/26545646/pexels-photo-26545646/free-photo-of-copper-colored-metal-wall-panels.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}} style={{width:200, height: 300}} />
+        </>
     )
 }
 
