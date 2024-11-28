b77ab91be0e946e578c42412a38e1dfc5b748034
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-28 16:46:21 +0530

    pressable Component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { View, Pressable } from 'react-native';
 import Writing from './Writing';
 import Picture from './Picture';
 import Pressive from './Pressive';
@@ -6,10 +6,16 @@ import Scrolling from './Scrolling';
 
 export default function App() {
   return (
-    <View style={{flex:1,display:"flex",justifyContent:"center", alignItems:"center", backgroundColor:"cornflowerblue", padding: 50}}>
-      <Writing />
-      {/* <Picture /> */}
-    <Scrolling />
+    <View style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "cornflowerblue", padding: 50 }}>
+      <Pressable onPress={() => console.log(`pressed on writig`)}>
+        <Writing />
+      </Pressable>
+      <Pressable onLongPress={() => console.log('picture has long pressed')}>
+        <Picture />
+      </Pressable>
+      <Pressable onPressOut={() => console.log('scrolling was pressed out')}>
+        <Scrolling />
+      </Pressable>
       <Pressive />
     </View>
   )
